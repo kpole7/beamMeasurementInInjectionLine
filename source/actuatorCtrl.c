aac7f63976dcd136e2a1db909a90b44344b13144
kpole7 <kpole@slcj.uw.edu.pl>
2026-05-28 11:52:30 +0200

    Refactor actuator control state management and add debug output for state changes

--- source/actuatorCtrl.c
+++ source/actuatorCtrl.c
@@ -4,6 +4,7 @@
 #include "actuatorCtrl.h"
 #include "sharedData.h"
 #include "pico/stdlib.h"
+#include <stdio.h>
 
 //---------------------------------------------------------------------------------------------------
 // Macro directives
@@ -19,42 +20,42 @@
 // Local variables
 //---------------------------------------------------------------------------------------------------
 
-static bool OldStateValveActuator1;
-static bool OldStateValveActuator2;
-static bool OldStateMotorActuatorIn;
-static bool OldStateMotorActuatorOut;
-static bool OldStateMotorActuatorBrake;
+static bool StateValveActuator1;
+static bool StateValveActuator2;
+static bool StateMotorActuatorIn;
+static bool StateMotorActuatorOut;
+static bool StateMotorActuatorBrake;
 
 //---------------------------------------------------------------------------------------------------
 // Function definitions
 //---------------------------------------------------------------------------------------------------
 
 void initializeActuatorControl(void) {
-	OldStateValveActuator1 = false;
-	OldStateValveActuator2 = false;
-	OldStateMotorActuatorIn = false;
-	OldStateMotorActuatorOut = false;
-	OldStateMotorActuatorBrake = false;
+	StateValveActuator1 = false;
+	StateValveActuator2 = false;
+	StateMotorActuatorIn = false;
+	StateMotorActuatorOut = false;
+	StateMotorActuatorBrake = false;
 
 	gpio_init(GPIO_FOR_VALVE_ACTUATOR_1);
 	gpio_set_dir(GPIO_FOR_VALVE_ACTUATOR_1, GPIO_OUT);
-	gpio_put(GPIO_FOR_VALVE_ACTUATOR_1, OldStateValveActuator1);
+	gpio_put(GPIO_FOR_VALVE_ACTUATOR_1, StateValveActuator1);
 
 	gpio_init(GPIO_FOR_VALVE_ACTUATOR_2);
 	gpio_set_dir(GPIO_FOR_VALVE_ACTUATOR_2, GPIO_OUT);
-	gpio_put(GPIO_FOR_VALVE_ACTUATOR_2, OldStateValveActuator2);
+	gpio_put(GPIO_FOR_VALVE_ACTUATOR_2, StateValveActuator2);
 
 	gpio_init(GPIO_FOR_MOTOR_ACTUATOR_IN);
 	gpio_set_dir(GPIO_FOR_MOTOR_ACTUATOR_IN, GPIO_OUT);
-	gpio_put(GPIO_FOR_MOTOR_ACTUATOR_IN, OldStateMotorActuatorIn);
+	gpio_put(GPIO_FOR_MOTOR_ACTUATOR_IN, StateMotorActuatorIn);
 
 	gpio_init(GPIO_FOR_MOTOR_ACTUATOR_OUT);
 	gpio_set_dir(GPIO_FOR_MOTOR_ACTUATOR_OUT, GPIO_OUT);
-	gpio_put(GPIO_FOR_MOTOR_ACTUATOR_OUT, OldStateMotorActuatorOut);
+	gpio_put(GPIO_FOR_MOTOR_ACTUATOR_OUT, StateMotorActuatorOut);
 
 	gpio_init(GPIO_FOR_MOTOR_ACTUATOR_BRAKE);
 	gpio_set_dir(GPIO_FOR_MOTOR_ACTUATOR_BRAKE, GPIO_OUT);
-	gpio_put(GPIO_FOR_MOTOR_ACTUATOR_BRAKE, OldStateMotorActuatorBrake);
+	gpio_put(GPIO_FOR_MOTOR_ACTUATOR_BRAKE, StateMotorActuatorBrake);
 }
 
 void actuatorCtrlTick(void) {
@@ -70,105 +71,125 @@ void actuatorCtrlTick(void) {
 	bool trigMotorActuatorOut = ModbusCoilTrigger[coilIndexFromAddress(MODBUS_ADDR_ACTUATOR3_CONTROL_OUT)];
 	bool trigMotorActuatorBrake = ModbusCoilTrigger[coilIndexFromAddress(MODBUS_ADDR_ACTUATOR3_CONTROL_BRAKE)];
 
+	// just for testing purposes
+	bool anyTrigger = trigValveActuator1 || trigValveActuator2 || trigMotorActuatorIn || trigMotorActuatorOut || trigMotorActuatorBrake;
+	if (((ModbusHoldingRegisters[79] & 0x4000u) != 0u) && anyTrigger) {
+		printf("Actuator:   1    2   3in  out brake\r\n");
+		printf("Old state:  %u    %u    %u    %u    %u\r\n", StateValveActuator1, StateValveActuator2, StateMotorActuatorIn, StateMotorActuatorOut, StateMotorActuatorBrake);
+		printf("Trigger:    %u    %u    %u    %u    %u\r\n", trigValveActuator1, trigValveActuator2, trigMotorActuatorIn, trigMotorActuatorOut, trigMotorActuatorBrake);
+		printf("Command:    %u    %u    %u    %u    %u\r\n", stateValveActuator1, stateValveActuator2, stateMotorActuatorIn, stateMotorActuatorOut, stateMotorActuatorBrake);
+	} // just for testing purposes
+
 	if (trigValveActuator1) {
+		ModbusCoilTrigger[coilIndexFromAddress(MODBUS_ADDR_ACTUATOR1_CONTROL)] = false;
 		gpio_put(GPIO_FOR_VALVE_ACTUATOR_1, stateValveActuator1); // Pneumatic valve #1
-		OldStateValveActuator1 = stateValveActuator1;
+		StateValveActuator1 = stateValveActuator1;
 	}
 
 	if (trigValveActuator2) {
+		ModbusCoilTrigger[coilIndexFromAddress(MODBUS_ADDR_ACTUATOR2_CONTROL)] = false;
 		gpio_put(GPIO_FOR_VALVE_ACTUATOR_2, stateValveActuator2); // Pneumatic valve #2
-		OldStateValveActuator2 = stateValveActuator2;
+		StateValveActuator2 = stateValveActuator2;
 	}
 
 	if (trigMotorActuatorIn) {
+		ModbusCoilTrigger[coilIndexFromAddress(MODBUS_ADDR_ACTUATOR3_CONTROL_IN)] = false;
 		if (stateMotorActuatorIn){
 			// 'insert' command
 
-			if (!OldStateMotorActuatorOut && !stateMotorActuatorBrake) { 
+			if (!StateMotorActuatorOut && !StateMotorActuatorBrake) { 
 				// only allow activation if not already activated in the opposite direction and not braking
 
 				// make sure the opposite direction is deactivated
 				gpio_put(GPIO_FOR_MOTOR_ACTUATOR_OUT, false);
-				OldStateMotorActuatorOut = false;
+				StateMotorActuatorOut = false;
 
 				// make sure the breaking is deactivated
 				gpio_put(GPIO_FOR_MOTOR_ACTUATOR_BRAKE, true); // low-level activation
-				OldStateMotorActuatorBrake = false;
+				StateMotorActuatorBrake = false;
 
 				// activate the 'insert' command
 				gpio_put(GPIO_FOR_MOTOR_ACTUATOR_IN, true);
-				OldStateMotorActuatorIn = true;
+				StateMotorActuatorIn = true;
 			}
 			else{
 				// emergency stop
 				gpio_put(GPIO_FOR_MOTOR_ACTUATOR_IN, false);
-				OldStateMotorActuatorIn = false;
+				StateMotorActuatorIn = false;
 				gpio_put(GPIO_FOR_MOTOR_ACTUATOR_OUT, false);
-				OldStateMotorActuatorOut = false;
+				StateMotorActuatorOut = false;
 				gpio_put(GPIO_FOR_MOTOR_ACTUATOR_BRAKE, true); // low-level activation
-				OldStateMotorActuatorBrake = false;
+				StateMotorActuatorBrake = false;
 			}
 		}
 		else{
 			// stop 'insert' command
 			gpio_put(GPIO_FOR_MOTOR_ACTUATOR_IN, false);
-			OldStateMotorActuatorIn = false;
+			StateMotorActuatorIn = false;
 		}
 	}
 
 	if (trigMotorActuatorOut) {
+		ModbusCoilTrigger[coilIndexFromAddress(MODBUS_ADDR_ACTUATOR3_CONTROL_OUT)] = false;
 		if (stateMotorActuatorOut){
 			// 'extract' command
 
-			if (!OldStateMotorActuatorIn && !stateMotorActuatorBrake) { 
+			if (!StateMotorActuatorIn && !StateMotorActuatorBrake) { 
 				// only allow activation if not already activated in the opposite direction and not braking
 
 				// make sure the opposite direction is deactivated
 				gpio_put(GPIO_FOR_MOTOR_ACTUATOR_IN, false);
-				OldStateMotorActuatorIn = false;
+				StateMotorActuatorIn = false;
 
 				// make sure the breaking is deactivated
 				gpio_put(GPIO_FOR_MOTOR_ACTUATOR_BRAKE, true); // low-level activation
-				OldStateMotorActuatorBrake = false;
+				StateMotorActuatorBrake = false;
 
 				// activate the 'extract' command
 				gpio_put(GPIO_FOR_MOTOR_ACTUATOR_OUT, true);
-				OldStateMotorActuatorOut = true;
+				StateMotorActuatorOut = true;
 			}
 			else{
 				// emergency stop
 				gpio_put(GPIO_FOR_MOTOR_ACTUATOR_IN, false);
-				OldStateMotorActuatorIn = false;
+				StateMotorActuatorIn = false;
 				gpio_put(GPIO_FOR_MOTOR_ACTUATOR_OUT, false);
-				OldStateMotorActuatorOut = false;
+				StateMotorActuatorOut = false;
 				gpio_put(GPIO_FOR_MOTOR_ACTUATOR_BRAKE, true); // low-level activation
-				OldStateMotorActuatorBrake = false;
+				StateMotorActuatorBrake = false;
 			}
 		}
 		else{
 			// stop 'extract' command
 			gpio_put(GPIO_FOR_MOTOR_ACTUATOR_OUT, false);
-			OldStateMotorActuatorOut = false;
+			StateMotorActuatorOut = false;
 		}
 	}
 	
 	if (trigMotorActuatorBrake) {
+		ModbusCoilTrigger[coilIndexFromAddress(MODBUS_ADDR_ACTUATOR3_CONTROL_BRAKE)] = false;
 		gpio_put(GPIO_FOR_MOTOR_ACTUATOR_IN, false);
-		OldStateMotorActuatorIn = false;
+		StateMotorActuatorIn = false;
 
 		gpio_put(GPIO_FOR_MOTOR_ACTUATOR_OUT, false);
-		OldStateMotorActuatorOut = false;
+		StateMotorActuatorOut = false;
 
 		if (stateMotorActuatorBrake) {
 			// command 'brake'
 			gpio_put(GPIO_FOR_MOTOR_ACTUATOR_BRAKE, !stateMotorActuatorBrake); // low-level activation
-			OldStateMotorActuatorBrake = stateMotorActuatorBrake;
+			StateMotorActuatorBrake = stateMotorActuatorBrake;
 		}
 		else{
 			// release brake
 			gpio_put(GPIO_FOR_MOTOR_ACTUATOR_BRAKE, true); // low-level activation
-			OldStateMotorActuatorBrake = false;
+			StateMotorActuatorBrake = false;
 		}
 	}
+
+	// just for testing purposes
+	if (((ModbusHoldingRegisters[79] & 0x4000u) != 0u) && anyTrigger) {
+		printf("New state:  %u    %u    %u    %u    %u\r\n", StateValveActuator1, StateValveActuator2, StateMotorActuatorIn, StateMotorActuatorOut, StateMotorActuatorBrake);
+		printf("\r\n");
+	} // just for testing purposes
 }
 
